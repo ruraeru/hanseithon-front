64038105e2a26ef2df1a244faaa98767c2dc04d6
hwangtaewoo <f93335383@gmail.com>
2020-11-30 16:17:15 +0900

    font add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+
+const Box = styled.div`
+  width: 1920px;
+  height: 1368px;
+  background-color: ${props => props.color};
+  background-image: url(./images/background1.png);
+`;
+
+const Line = styled.div`
+  width: 12rem;
+  height: 0;
+  margin: 12.5px 20.5px 0 0;
+  background-color: white;
+`;
+
+const Text = styled.text`
+  font-family: "KoreanJMDBR";
+  src: url("./fonts/KoreanJMDBR.ttf") format("ttf");
+  color: #ffffff;
+  font-size: 25px;
+  line-height: 34px;
+  width: 368px;
+  height: 25px;
+  margin: 0 20.5px;
+  text-align: left;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Box>
+      <Text>
+        The Third Session of 2020
+      </Text>
+    </Box>
+    <Box color="blue" />
+    <Box color="red" /> 
+    <Box color="orange" />
+    </>
   );
 }
 
