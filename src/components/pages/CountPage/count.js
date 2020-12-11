32d33dbb548679a405def4d1b0519e5a872eb05d
hwangtaewoo <f93335383@gmail.com>
2020-12-12 04:32:53 +0900

    count end message modify

--- src/components/pages/CountPage/count.js
+++ src/components/pages/CountPage/count.js
@@ -46,7 +46,7 @@ function Counter() {
   });
   return (
     <div>
-      {timerComponents.length ? timerComponents : <span>제한 시간이 초과되었습니다!!!</span>}
+      {timerComponents.length ? timerComponents : <h5>제출 마감되었습니다!</h5>}
     </div>
   );
 }
